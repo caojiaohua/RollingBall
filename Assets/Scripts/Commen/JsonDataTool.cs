4580cf5225f2bd0ecaefa3dc6db5a70ca33be354
caojiaohua <2632156180@qq.com>
2020-11-30 14:42:04 +0800

    美术资源

---
+++ Assets/Scripts/Commen/JsonDataTool.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using System.IO;
+using System.Text;
+using System;
+
+
+/*
+ * 
+ * litjson 要求json数据的属性名要和实体类中的变量名一一对应
+ */
+public class JsonDataTool
+{
+
+    public static List<T> GetListFromJson<T>(string path)
+    {//获取json数据
+        string str = File.ReadAllText(appSetting.dataPath + path+".json", Encoding.GetEncoding("UTF-8"));//读取Json字符串
+        if (str == null) Debug.LogError("未找到目标资源:" + path+".json");
+        List<T> list = JsonUtility.FromJson<Serialization<T>>(str).ToList();
+        return list;
+    }
+
+    public static void SetJsonFromList<T>(string path, List<T> list)//此方法用于存档的实现
+    {//修改json数据
+        string jsonstr = JsonMapper.ToJson(list);
+        File.WriteAllText(appSetting.dataPath + path + ".json","{"+'"'+"target"+'"'+":"+ jsonstr +"}");//  {"target": [      ////      	]}
+}
+
+    public static bool isFileExit()
+    {
+        return false;
+    }
+}
+
+[Serializable]
+public class Serialization<T>
+{
+    [SerializeField]
+    List<T> target;
+    public List<T> ToList() { return target; }
+
+    public Serialization(List<T> target)
+    {
+        this.target = target;
+    }
+}
+
---
+++ Assets/Scripts/Manager/appSetting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class appSetting : MonoBehaviour
+{
+
+
+#if UNITY_IOS || UNITY_ANDROID
+    public static string dataPath = Application.streamingAssetsPath + "/";
+#elif UNITY_EDITOR
+    public static string dataPath = Application.streamingAssetsPath + "/";
+#endif
+
+
+    #region  等级颜色
+    public static Color level_A = Color.red;//new Color(136f/255f,77/255f,126f/255f,255f/255f);
+    public static Color level_B = Color.magenta;
+    public static Color level_C = Color.yellow;
+    public static Color level_D = Color.green;
+    public static Color level_S = Color.blue;
+    public static Color level_Splus = Color.cyan;
+    #endregion
+}
